be92eeef4caddac01c049ab48fbb614b47628f5f
KMN Data Center <admin@kmn.ie>
2026-01-26 11:51:04 +0000

    Update snmpd.sh

--- Scripts/snmpd.sh
+++ Scripts/snmpd.sh
@@ -4,3 +4,6 @@ sudo apt install snmpd snmp libsnmp-dev
 cp /etc/snmp/snmpd.conf /etc/snmp/snmpd.bak
 # Copy minimalist file
 cp ./snmpd.conf /etc/snmp/snmpd.conf
+
+echo 'After editing the conf file'
+Enable and start the service using systemctl
